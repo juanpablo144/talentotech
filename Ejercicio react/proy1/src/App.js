ae68f0dccf1415b23d63d3d8eb0c54e1a93df292
juan pablo <juanpabloperez990215@gmail.com>
2024-06-12 18:44:19 -0500

    update react example

--- Ejercicio react/proy1/src/App.js
+++ Ejercicio react/proy1/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
+  const cd = new Date();
 
-function App() {
+  const colorStyle = {
+    color: props.color,
+    fontSize: props.size
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={colorStyle}>
+    <h1>Hola Clase</h1>
+    <h3>Hora: {cd.toLocaleTimeString()}</h3>
+   </div>
   );
 }
 
