e2e32331466b586aaab4963dc98d321ca01308be
juan pablo <juanpabloperez990215@gmail.com>
2024-06-12 19:51:16 -0500

    update react example

--- Ejercicio react/proy1/src/App.js
+++ Ejercicio react/proy1/src/App.js
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 function App(props) {
+
+  const [cont, setCont] = useState(3);
+
+  let incCont = () => {
+    setCont(cont + 1)
+  }
+
+  let decCont = () => {
+    setCont(cont - 1)
+  }
+
   const cd = new Date();
 
-  const colorStyle = {
-    color: props.color,
-    fontSize: props.size
+  const estilo = {
+    color: props.c,
+    fontSize: props.t
   };
 
 
   return (
-   <div style={colorStyle}>
-    <h1>Hola Clase</h1>
+   <div style={estilo}>
+    <h1>Hola Clase - {cont}</h1>
     <h3>Hora: {cd.toLocaleTimeString()}</h3>
+    <button onClick={incCont}>Incrementar</button>
+    <button onClick={decCont}>Decrementar</button>
    </div>
   );
 }
--- Ejercicio react/proy1/src/index.js
+++ Ejercicio react/proy1/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App color="green" size="20px"/>
-  </React.StrictMode>
+  <div>
+    <App c="green" t="22px" />
+    <App c="red" t="20px" />
+  </div>
 );
