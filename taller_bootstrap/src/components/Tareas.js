3ab6e27eea539081be474ce30b4bc879e4f933b6
juanpablo144 <juanpabloperez990215@gmail.com>
2024-06-24 20:05:09 -0500

    update changes bootstrap

--- taller_bootstrap/src/components/Tareas.js
+++ taller_bootstrap/src/components/Tareas.js
@@ -11,6 +11,17 @@ function Tareas() {
           <Col>Col 1 de 2 </Col>
           <Col>Col 2 de 2 </Col>
         </Row>
+        <Row>
+          <Col>Col 1 de 3 </Col>
+          <Col>Col 2 de 3 </Col>
+          <Col>Col 3 de 3 </Col>
+        </Row>
+        <Row>
+          <Col>Col 1 de 4 </Col>
+          <Col>Col 2 de 4 </Col>
+          <Col>Col 3 de 4 </Col>
+          <Col>Col 4 de 4 </Col>
+        </Row>
       </Container>
       <h1>Hola clase</h1>
       <p className="text-info">Este es un subtitulo</p>
