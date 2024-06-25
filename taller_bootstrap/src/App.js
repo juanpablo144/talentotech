988a6780997c5f1edd464cea6c1b3a3170d47818
juanpablo144 <juanpabloperez990215@gmail.com>
2024-06-24 19:24:38 -0500

    update changes bootstrap

---
+++ taller_bootstrap/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{" "}
+    </>
+  );
+}
+
+export default App;
