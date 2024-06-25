93a64971d7dc12157a0eef89d1902c7932b80d6e
juanpablo144 <juanpabloperez990215@gmail.com>
2024-06-25 18:37:58 -0500

    update changes bootstrap

--- taller_bootstrap/src/components/Tareas.js
+++ taller_bootstrap/src/components/Tareas.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Menu } from "./Menu";
 import { Image, Container, Row, Col } from "react-bootstrap";
+import "./Tareas.css";
 
 function Tareas() {
   return (
     <>
       <Menu />
-      <Container fluid={true}>
-        <Row>
-          <Col>Col 1 de 2 </Col>
-          <Col>Col 2 de 2 </Col>
+      <Container fluid={false}>
+        <Row className="justify-content-center">
+          <Col sm={2} className="celda">
+            Col 1 de 2{" "}
+          </Col>
+          <Col sm={2} className="celda">
+            Col 2 de 2{" "}
+          </Col>
         </Row>
         <Row>
-          <Col>Col 1 de 3 </Col>
-          <Col>Col 2 de 3 </Col>
-          <Col>Col 3 de 3 </Col>
+          <Col className="celda">Col 1 de 3 </Col>
+          <Col xs={6} className="celda">
+            Col 2 de 3{" "}
+          </Col>
+          <Col className="celda">Col 3 de 3 </Col>
         </Row>
         <Row>
-          <Col>Col 1 de 4 </Col>
-          <Col>Col 2 de 4 </Col>
-          <Col>Col 3 de 4 </Col>
-          <Col>Col 4 de 4 </Col>
+          <Col className="celda">Col 1 de 4 </Col>
+          <Col className="celda">Col 2 de 4 </Col>
+          <Col lg={4} className="celda">
+            Col 3 de 4{" "}
+          </Col>
+          <Col className="celda">Col 4 de 4 </Col>
         </Row>
       </Container>
       <h1>Hola clase</h1>
