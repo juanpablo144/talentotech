0e62c44ff3d5e073c261d06c81f9c592fbba0479
juanpablo144 <juanpabloperez990215@gmail.com>
2024-06-24 20:01:52 -0500

    update changes bootstrap

--- taller_bootstrap/src/components/Tareas.js
+++ taller_bootstrap/src/components/Tareas.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Menu } from "./Menu";
+import { Image, Container, Row, Col } from "react-bootstrap";
 
 function Tareas() {
   return (
     <>
       <Menu />
+      <Container>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
       <h1>Hola clase</h1>
       <p className="text-info">Este es un subtitulo</p>
+      <Image
+        src="/assets/images/install-react.jpg"
+        alt="react"
+        className="w-50"
+        roundedCircle
+        fluid
+      />
     </>
   );
 }
